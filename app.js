209646fecfa23921ae1f32c105cd7e8b49ef8175
J Valentin <jerber.valentin@gmail.com>
2025-01-31 12:21:58 -0500

    versio final

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaDeNumerosSecretos = [];
+let maximoAleatorio = 10;
 
 function asignarTextoAetiqueta(etiqueta, texto) {
     //las variables q se crean aqui se ponen en la cabecera por defecto
@@ -9,30 +11,50 @@ function asignarTextoAetiqueta(etiqueta, texto) {
 }
 
 function generadorDeNumeroAleatorio() {
-    return Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*maximoAleatorio+1);
+    //console.log(numeroGenerado);
+    //console.log(listaDeNumerosSecretos);
+
+    if (listaDeNumerosSecretos.length == maximoAleatorio) {
+        asignarTextoAetiqueta('p', 'Ya salieron todos los número posibles, reiniciar el juego');
+        document.getElementById('intentar').setAttribute('disabled', true);
+    } else {
+        //!Si el numero generado esta en la lista, generar otro con recursividad
+        if (listaDeNumerosSecretos.includes(numeroGenerado)) {
+            return generadorDeNumeroAleatorio();
+        } else {
+            listaDeNumerosSecretos.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
 
-console.log(numeroSecreto);
+//console.log(numeroSecreto);
 
 function botonIntentoDeUsuario() {
     //El valor del numero q inrgesa el usuario en INPUT de html y solo coge el valor(value)
     let numeroDeUsuario = document.getElementById('valorIngresadoDeUsuario').value;
-    if (numeroDeUsuario == numeroSecreto) {
-        asignarTextoAetiqueta('p', `Lo Lograste en ${intentos} ${intentos == 1? 'vez':'veces'}`);
-        //para activar l boton NUEVO JUEGO al remover el atributo DISABLED q tenia
-        //! document.getElementById('reiniciar').removeAttribute('disabled');
-        document.querySelector('#reiniciar').removeAttribute('disabled');
-    } else {        
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoAetiqueta('p', 'El número secreto es menor');
-        } else {
-            asignarTextoAetiqueta('p', 'El número secreto es mayor');
-        }
-    }
-    intentos++;
-    limpiarInput();
 
-    console.log(numeroDeUsuario);
+    if (numeroDeUsuario > maximoAleatorio || numeroDeUsuario < 1) {
+        asignarTextoAetiqueta('p', `Recuerda que el número es del 1-${maximoAleatorio}`);
+    } else {
+        if (numeroDeUsuario == numeroSecreto) {
+            asignarTextoAetiqueta('p', `Lo Lograste en ${intentos} ${intentos == 1? 'vez':'veces'}`);
+            document.getElementById('intentar').setAttribute('disabled', true);
+            //para activar l boton NUEVO JUEGO al remover el atributo DISABLED q tenia
+            //! document.getElementById('reiniciar').removeAttribute('disabled');
+            document.querySelector('#reiniciar').removeAttribute('disabled');
+        } else {        
+            if (numeroDeUsuario > numeroSecreto) {
+                asignarTextoAetiqueta('p', 'El número secreto es menor');
+            } else {
+                asignarTextoAetiqueta('p', 'El número secreto es mayor');
+            }
+        }
+        intentos++;
+    }    
+    limpiarInput();
+    //console.log(numeroDeUsuario);
 }
 
 function limpiarInput() {
@@ -55,9 +77,11 @@ function botonReiniciarJuego() {
 function condicionesIniciales() {
     //! para no repetir, lo ponemos en una funcion q inicialice y luego lo invocamos fuera
     asignarTextoAetiqueta('h1', 'Juego del número secreto');
-    asignarTextoAetiqueta('p', 'Introduzca un número de 1-10');
+    asignarTextoAetiqueta('p', `Introduzca un número del 1-${maximoAleatorio}`);
     numeroSecreto = generadorDeNumeroAleatorio();
     intentos = 1;
+    //activacion de boton inentar
+    document.getElementById('intentar').removeAttribute('disabled');
 }
 //!aqui estamos invocando condiciones iniciales
 condicionesIniciales();
